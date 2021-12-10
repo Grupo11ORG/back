4f638c69556d9b24ab84cb9a777715f3816a86a4
NicoFoletto <marcelo.niko98@gmail.com>
2021-12-10 11:01:53 -0300

    descomente rutas

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.set("port", process.env.PORT || 4000);
 app.use("/api/profesionales", require("./src/routes/professionals.routes"));
 // rutas para registrar un usuario y autenticarlo
 app.use("/api", require("./src/routes/apiAuthentication.routes"));
-// app.use("/user", require("./src/routes/user.routes"));
-// app.use("/api", require("./src/routes/api.routes"));
-// app.use("/rol", require("./src/routes/rol.routes"));
+ app.use("/user", require("./src/routes/user.routes"));
+ app.use("/api", require("./src/routes/api.routes"));
+ app.use("/rol", require("./src/routes/rol.routes"));
 
 app.listen(app.get("port"), () =>
   console.log(`Example app listening on port ${app.get("port")}!`)
